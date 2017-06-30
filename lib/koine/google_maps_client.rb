801ee57c4dc3eefc28feff6f9d4f9e6be036781d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-06-30 22:20:44 +0200

    Initial commit

---
+++ lib/koine/google_maps_client.rb
@@ -0,0 +1,7 @@
+require "koine/google_maps_client/version"
+
+module Koine
+  module GoogleMapsClient
+    # Your code goes here...
+  end
+end
---
+++ lib/koine/google_maps_client/version.rb
@@ -0,0 +1,5 @@
+module Koine
+  module GoogleMapsClient
+    VERSION = "0.1.0"
+  end
+end
